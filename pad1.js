418e79f9b934cc4744370041edd8b906ad6e5f94
Alex Auch <alexsamuelauch@gmail.com>
2024-08-15 08:18:17 -0600

    initial commit/went over javascript variables and datatypes

---
+++ pad1.js
@@ -0,0 +1,30 @@
+/* 08/14/24
+Javascript is a programming language of the web.
+
+A framework is an abstraction on top of javascript.
+
+Javascript is not the same as Java.
+
+A programming language is a group of symbols/tokens that, when
+combined, can be translated by a compiler into machine code.
+
+A compiler is simply another program/code that translates other
+code for a computer to understand.
+
+**The Elements of Computing Systems** A book to understand computing
+
+Runtime is an environment where code executes. Javascript can run
+in the backend via node.js as well as running in the frontend via the browser.
+These runtimes utilize an engine, like V8, to run the code.
+
+"You Don't Know Javascript" by Kyle Simpson. Github-> getify
+
+*/
+
+//Variables and Datatypes in Javascript
+
+const yourFirstVariable = 'Learning to code gives you superpowers.';
+const yourSecondVariable = 10;
+const yourThirdVariable = { firstProperty: 'hello world' }
+
+console.log(yourFirstVariable)
